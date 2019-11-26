dd097785edd7a9143946d47eab3281cc068c3710
Linus Karlsson <linus@zozs.se>
2019-11-26 16:38:33 +0100

    Perhaps apt-get doesnt mess with output tty as much?

--- ci/compilerun.sh
+++ ci/compilerun.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Install dependencies (to save time this could be done in a prepared image instead...)
-apt update
-apt install -y build-essential
+apt-get update
+apt-get install -y build-essential
 
 # Now actually build our app.
 make
