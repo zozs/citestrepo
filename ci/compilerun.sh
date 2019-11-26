928827143fc1be4a60c4986fd76986f031edbd86
Linus Karlsson <linus@zozs.se>
2019-11-26 16:34:27 +0100

    We actually need a compiler inside the image.

--- ci/compilerun.sh
+++ ci/compilerun.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+# Install dependencies (to save time this could be done in a prepared image instead...)
+apt update
+apt install -y build-essential
+
+# Now actually build our app.
 make
 ./main
